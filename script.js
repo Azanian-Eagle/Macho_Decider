5db9969079a7a144ed04730e722777ca30868d81
Romeo Mngqibisa <102858864+rmngqibisa@users.noreply.github.com>
2024-08-24 16:42:57 +0200

    Update script.js
    Signed-off-by: Romeo Mngqibisa <102858864+rmngqibisa@users.noreply.github.com>

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('decide-button').addEventListener('click', function () {
     const option2 = document.getElementById('option2').value;
 
     if (option1 === "" || option2 === "") {
-        document.getElementById('result').textContent = "ENTER BOTH OPTIONS LIKE A REAL MAN!";
+        document.getElementById('result').textContent = "C'MON, DON'T BE A POES MAN! ENTER BOTH OPTIONS!";
     } else {
         const choices = [option1, option2];
         const randomChoice = choices[Math.floor(Math.random() * choices.length)];
